14b9150dd0ab60f9c9ccaa341cef4b9d8eeb1bf6
wolfy <svstar94@naver.com>
2020-08-17 19:56:12 +0900

    20200817 방송 편성표 데이터 수집

--- Colect_Data/Colect_Data/spiders/instacrawl.py
+++ Colect_Data/Colect_Data/spiders/instacrawl.py
@@ -1,9 +1,5 @@
-import time
 import scrapy
-import re
 from datetime import datetime
-import pandas as pd
-import tqdm
 import json
 
 # Insta Cralwer
@@ -20,10 +16,12 @@ class ExampleSpider(scrapy.Spider):
     end_cursor=True
     def parse(self, response):
         text = response.css('::text').get()
-        text.replace('\n', ' ')
-        sources = json.loads(text)['data']['hashtag']['edge_hashtag_to_media']['edges'] #필요한 데이터
+        yield { 'text' : text }
+        return
+        # text.replace('\n', ' ')
+        sources = json.loads(text)['data']['hashtag']['edge_hashtag_to_media'] #필요한 데이터
 
-        for source in sources:
+        for source in sources['edges']:
             try:
                 yield {
                     'date' : datetime.fromtimestamp(source['node']['taken_at_timestamp']),
@@ -32,7 +30,9 @@ class ExampleSpider(scrapy.Spider):
             except:
                 pass
 
-        self.end_cursor = json.loads(text)['data']['hashtag']['edge_hashtag_to_media']['page_info']['end_cursor'] #Next Page 확인
-        if self.end_cursor != None:
+        ExampleSpider.end_cursor = sources['page_info']['end_cursor'] #Next Page 확인
+        if ExampleSpider.end_cursor != None:
             URL = 'http://instagram.com/graphql/query/?query_hash=7dabc71d3e758b1ec19ffb85639e427b&variables={"tag_name":"' + self.search_word + '","first":12'+',"after":"'+self.end_cursor+'"}'
             yield scrapy.Request(URL, callback=self.parse)
+        else :
+            return
