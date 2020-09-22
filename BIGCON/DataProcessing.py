97f13c4f3f05ed3b7ffe963343981835de11d886
wolfy <svstar94@naver.com>
2020-09-22 23:06:38 +0900

    20200922 데이터 정리, ipynb 파일 통합

--- BIGCON/DataProcessing.py
+++ BIGCON/DataProcessing.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import datetime
 
-def date_change(datas, target_col = 1, date_format = '%Y-%m-%d %H:%M'):
-    datas[datas.columns[target_col]] = datas[datas.columns[target_col]].apply(lambda i : datetime.datetime.strptime(i, date_format))
+def date_change(datas, target_col = 'date', date_format = '%Y-%m-%d %H:%M'):
+    datas[target_col] = datas[target_col].apply(lambda i : datetime.datetime.strptime(i, date_format))
     return datas
